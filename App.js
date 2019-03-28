774ea51472ca96a35b5930697e9ef4672baa661a
WY <845283528@qq.com>
2019-03-28 21:11:51 +0800

    更新

--- App.js
+++ App.js
@@ -20,7 +20,6 @@ const instructions = Platform.select({
 });
 
 
-
 type Props = {};
 export default class App extends Component<Props> {
   render() {
@@ -34,31 +33,33 @@ export default class App extends Component<Props> {
           {/*<Text style={styles.btn}>连接打印机</Text>*/}
           {/*</TouchableOpacity>*/}
 
-            <TouchableOpacity onPress={() => {
-                Print.print(1,
-                    '[{"reserved_no":"12345678901234567890",' +
-                    'tray_serlno_label:"12345678901234567890","item_num_id":"12345678901234567890",' +
-                    '"barcode":"12345678901234567890","item_name":"维维熊","qty":"21","main_distribution_type":1}]')
-            }
+          <TouchableOpacity onPress={() => {
+            Print.print(1,
+                '[{"reserved_no":"12345678901234567890",' +
+                'tray_serlno_label:"12345678901234567890","item_num_id":"12345678901234567890","supplyer":"供应商A",' +
+                '"barcode":"12345678901234567890","item_name":"维维熊","qty":"21","main_distribution_type":1}]')
+          }
 
-            } activeOpacity={0.2} focusedOpacity={0.5}>
-                <Text style={styles.btn}>打印入库装箱单</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => Print.print(2,
-                '[{"barcode":"11111111111","main_distribution_type":"新品","remain_qty":"123","sow_zone_qty_list":' +
-                '[{"zone_num_id":"S","sow_num_id":"91","qty":"20"},{"zone_num_id":"S","sow_num_id":"91","qty":"20"},' +
-                '{"zone_num_id":"S","sow_num_id":"91","qty":"20"},{"zone_num_id":"S","sow_num_id":"91","qty":"20"},' +
-                '{"zone_num_id":"S","sow_num_id":"91","qty":"20"}]}]'
-            )}
-                              activeOpacity={0.2} focusedOpacity={0.5}>
-                <Text style={styles.btn}>打印箱分货清单</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => Print.print(3,
-                '{"code": 0,"container_labserlno": "190226000004", "full_message": "", "message": "成功","physical_num_id": 1167,"rec_date": 1550678400000, "rec_physical_num_id": 300101,"reserved_no": "131902260040001"}'
-            )}
-                              activeOpacity={0.2} focusedOpacity={0.5}>
-                <Text style={styles.btn}>打印出库箱唛头</Text>
-            </TouchableOpacity>
+          } activeOpacity={0.2} focusedOpacity={0.5}>
+            <Text style={styles.btn}>打印入库装箱单</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => Print.print(2,
+              '[{"barcode":"11111111111","main_distribution_type":"新品","remain_qty":"123","item_num_id":"12345678901234567890","itemid":"12345678901234567890","sow_zone_qty_list":' +
+              '[' +
+              // '{"zone_num_id":"S","sow_num_id":"91","qty":"20"},{"zone_num_id":"S","sow_num_id":"91","qty":"20"},' +
+              // '{"zone_num_id":"S","sow_num_id":"91","qty":"20"},{"zone_num_id":"S","sow_num_id":"91","qty":"20"},' +
+              '{"zone_num_id":"S","sow_num_id":"91","qty":"20"}]}]'
+          )}
+                            activeOpacity={0.2} focusedOpacity={0.5}>
+            <Text style={styles.btn}>打印箱分货清单</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => Print.print(3,
+              '{"code": 0,"container_labserlno": "190226000004", "full_message": "", "message": "成功","physical_num_id": 1167,"rec_date": 1550678400000, ' +
+              '"shipTranCompany":"德邦物流","rec_physical_num_id": 300101,"reserved_no": "131902260040001"}'
+          )}
+                            activeOpacity={0.2} focusedOpacity={0.5}>
+            <Text style={styles.btn}>打印出库箱唛头</Text>
+          </TouchableOpacity>
         </View>
     );
   }
